6b5ff5ae672208c08f8a54214254ba38bfa08d2b
Vinod Jagtap <vinod.jagtap@hotmail.com>
2020-06-24 20:52:55 +0530

    multiple image update

--- lib/screens/downloads.dart
+++ lib/screens/downloads.dart
@@ -1,9 +1,11 @@
-import 'dart:html';
+import 'dart:io';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'dart:io' as io;
 import 'package:ext_storage/ext_storage.dart';
 import 'package:video_thumbnail/video_thumbnail.dart';
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
 
 class Downloads extends StatefulWidget {
 
@@ -14,6 +16,7 @@ class Downloads extends StatefulWidget {
 class DownloadsState extends State<Downloads> {
   String directory;
   List files = new List();
+  List<VideoPlayerController> controllers = new List<VideoPlayerController>();
 
   @override
   void initState() {
@@ -24,34 +27,65 @@ class DownloadsState extends State<Downloads> {
   //Fetch files from app directory
   void _listofFiles() async {
     var directory = await ExtStorage.getExternalStorageDirectory();
-    print(directory);
     setState(() {
-      files = io.Directory("$directory/InstaSaver").listSync();
-      print(files);
+      var allFiles = io.Directory("$directory/InstaSaver").listSync();
+      for
     });
   }
 
-  void checkFileIsOfImageOrVideoType(File file) {
-    List<String> fileParts = file.toString().split('?').toList();
+  Future<Card> _getCard(File file) async {
+    List<String> fileParts = file.toString().split('.').toList();
     print(fileParts);
+    if (fileParts[1] == "mp4") {
+      final uint8list = await VideoThumbnail.thumbnailData(
+        video: file.path,
+        imageFormat: ImageFormat.JPEG,
+        maxWidth: 128, // specify the width of the thumbnail, let the height auto-scaled to keep the source aspect ratio
+        quality: 25,
+      );
+      return new Card(
+        elevation: 3,
+        child: Image.memory(uint8list),
+      );
+    } else {
+      return new Card(
+        elevation: 3,
+        child: Image.file(file),
+      );
+    }
+  }
+
+  _imageList() {
+    GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (_, index) {
+          return new Card(
+        elevation: 3,
+        child: Image.file(files[index],fit: BoxFit.fill,),
+      );
+        },
+        itemCount: files.length,
+      );
   }
 
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Downloads")
+        title: Text("Downloads"),
+        bottom: TabBar(tabs:
+        [
+          Tab(icon: Icon(Icons.image)),
+          Tab(icon: Icon(Icons.video_library),)
+        ]
+        ),
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (_, index) {
-          return new Card(
-            elevation: 3,
-            child: Image.file(files[index]),
-          );
-        },
-        itemCount: files.length,
+      body: TabBarView(children: 
+      [
+        _imageList()
+      ]
       ),
+
     );
   }
 }
